a4f97cfef543104c31bf35aa696197abdbe4dfda
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-10 21:22:09 +0300

    refactor(src) deleted unnecessary files

--- cypress/component/App.test.js
+++ cypress/component/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'cypress-react-unit-test';
-import App from '../../src/App';
+import App from '../../src/component/App';
 
 describe('App', () => {
 	it('renders', () => {
---
+++ src/component/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+	return <div>App Component</div>;
+}
+
+export { App as default };
