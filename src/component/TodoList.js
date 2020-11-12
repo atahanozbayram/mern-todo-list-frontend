d79c431a2494573a9a45d526dca8166e82cc1318
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-12 05:35:54 +0300

    refactor(component): TodoList

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import '../style/reset.css';
 
-function TodoList() {}
+function TodoList() {
+	return (
+		<div>
+			<ul></ul>
+		</div>
+	);
+}
+
+export { TodoList as default };
