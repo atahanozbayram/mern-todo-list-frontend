875e00c488b5d1f59bb64b73cc587259bd0722c9
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-12 05:13:40 +0300

    feat(component): TodoAdd component added

---
+++ src/component/TodoAdd.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../style/reset.css';
+
+function TodoAdd(props) {
+	return (
+		<div>
+			<div>
+				<div>
+					<label></label>
+				</div>
+				<div>
+					<input type="text" />
+				</div>
+				<div>
+					<button>ADD</button>
+				</div>
+			</div>
+		</div>
+	);
+}
