7d6a278821bbbadfa4369b7c13d2d37714b0fc96
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-12 04:40:21 +0300

    refactor(component): component added TodoHeader

---
+++ src/component/TodoHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/reset.css';
+
+function TodoHeader(props) {
+	return (
+		<div>
+			<h2>Todo List</h2>
+		</div>
+	);
+}
+
+export { TodoHeader as default };
