a87defff7d5fc41e0e6366ebae3576bc217e8fd6
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-13 07:36:55 +0300

    refactor(component): LoginPage redesigned

--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import LoginHeader from './LoginHeader';
+import LoginForm from './LoginForm';
+import '../style/reset.css';
 
 export default function LoginPage() {
-	return <div></div>;
+	return (
+		<div>
+			<LoginHeader />
+			<LoginForm />
+		</div>
+	);
 }
