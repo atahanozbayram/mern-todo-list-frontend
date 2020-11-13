d98f13dba822e997f8630dc125a2a87f28d7d72f
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-13 08:58:06 +0300

    refactor(component): Added style to LoginPage component

--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -2,10 +2,11 @@ import React from 'react';
 import LoginHeader from './LoginHeader';
 import LoginForm from './LoginForm';
 import '../style/reset.css';
+import styles from '../style/LoginPage.module.css';
 
 export default function LoginPage() {
 	return (
-		<div>
+		<div className={styles['LoginPage']}>
 			<LoginHeader />
 			<LoginForm />
 		</div>
