ffa6ca2f2e6d2c0311bce730bcf024fe951c862f
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-14 18:03:39 +0300

    refactor(component): RegisterPage's jsx is updated

--- src/component/RegisterPage.js
+++ src/component/RegisterPage.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import '../style/reset.css';
+import RegisterHeader from './RegisterHeader';
+import RegisterForm from './RegisterForm';
+import styles from '../style/RegisterPage.module.css';
 
-export default function RegisterPage() {
-	return <div></div>;
+function RegisterPage() {
+	return (
+		<div className={styles['RegisterPage']}>
+			<RegisterHeader />
+			<RegisterForm />
+		</div>
+	);
 }
+
+export { RegisterPage as default };
