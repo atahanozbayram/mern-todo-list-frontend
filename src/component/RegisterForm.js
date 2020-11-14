e5adf76d40910a5024fbdcb9e43d70b7130a6ad3
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-14 18:02:38 +0300

    refactor(component): RegisterForm jsx updated

--- src/component/RegisterForm.js
+++ src/component/RegisterForm.js
@@ -1,39 +1,77 @@
 import React from 'react';
+import '../style/reset.css';
+import styles from '../style/RegisterForm.module.css';
 
 function RegisterForm(props) {
 	return (
-		<div>
+		<div className={styles['RegisterForm']}>
 			<form>
 				<div>
-					<label>First name:</label>
-					<input type="text" name="firstName" />
+					<div>
+						<label>First name:</label>
+					</div>
+					<input
+						type="text"
+						name="firstName"
+						autoComplete="off"
+						className={styles['RegisterForm__input-text']}
+					/>
 				</div>
-
 				<div>
-					<label>Last name:</label>
-					<input type="text" name="lastName" />
+					<div>
+						<label>Last name:</label>
+					</div>
+					<input
+						type="text"
+						name="lastName"
+						autoComplete="off"
+						className={styles['RegisterForm__input-text']}
+					/>
 				</div>
-
 				<div>
-					<label>E-mail:</label>
-					<input type="email" name="email" />
+					<div>
+						<label>E-mail:</label>
+					</div>
+					<input
+						type="text"
+						name="email"
+						autoComplete="off"
+						className={styles['RegisterForm__input-text']}
+					/>
 				</div>
-
 				<div>
-					<label>E-mail confirmation:</label>
-					<input type="email" name="emailConfirmation" />
+					<div>
+						<label>E-mail confirmation:</label>
+					</div>
+					<input
+						type="text"
+						name="emailConfirmation"
+						autoComplete="off"
+						className={styles['RegisterForm__input-text']}
+					/>
 				</div>
-
 				<div>
-					<label>Password:</label>
-					<input type="password" name="password" />
+					<div>
+						<label>Password:</label>
+					</div>
+					<input
+						type="password"
+						name="password"
+						autoComplete="off"
+						className={styles['RegisterForm__input-text']}
+					/>
 				</div>
-
 				<div>
-					<label>Password confirmation:</label>
-					<input type="password" name="passwordConfirmation" />
+					<div>
+						<label>Password confirmation:</label>
+					</div>
+					<input
+						type="password"
+						name="passwordConfirmation"
+						autoComplete="off"
+						className={styles['RegisterForm__input-text']}
+					/>
 				</div>
-
 				<div>
 					<button>Register</button>
 				</div>
