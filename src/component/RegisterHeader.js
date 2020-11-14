d606768a457eed71f8f7861e2d209f0005bf06c6
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-11-14 18:03:07 +0300

    refactor(component): RegisterHeader added

---
+++ src/component/RegisterHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/reset.css';
+import generalStyles from '../style/general.module.css';
+
+function RegisterHeader() {
+	return (
+		<div className={generalStyles['header']}>
+			<h2>Register Page</h2>
+		</div>
+	);
+}
+
+export { RegisterHeader as default };
